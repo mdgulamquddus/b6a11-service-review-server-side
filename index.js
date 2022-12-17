30f0904be1a3d6b7781e7f7d67ea286e11f1c00b
md-gulam-quddus <sohag.1984@gmail.com>
2022-12-17 20:48:56 +0100

    limit cases api endpoint created

--- index.js
+++ index.js
@@ -9,6 +9,31 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.h6dt8.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const casesCollection = client.db("lawyerFirm").collection("cases");
+
+    // cases get 3 api
+    app.get("/casesLimt3", async (req, res) => {
+      const query = {};
+
+      const cursor = casesCollection.find(query);
+      const cases = await cursor.limit(3).toArray();
+      res.send(cases);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Lawyer Server is Running");
 });
